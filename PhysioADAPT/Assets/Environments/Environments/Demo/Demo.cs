66c78922e058864d580f40a687de09ef8946dcf3
Teresa Paulino <teresampaulino@hotmail.com>
2016-06-12 20:28:40 +0100

    Created simple demo scene for testing

---
+++ PhysioADAPT/Assets/Environments/Environments/Demo/Demo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Assets.Environments;
+
+public class Demo : MonoBehaviour
+{
+    private static List<UpdatableVar> UpdVariables = new List<UpdatableVar>();
+    private static List<ReadableVar> ReadVariables = new List<ReadableVar>();
+
+
+    public static void SetUpdatableVariable(UpdatableVar var)
+    {
+        UpdVariables.Add(var);
+    }
+
+    public static void SetReadableVariable(ReadableVar var)
+    {
+        ReadVariables.Add(var);
+    }
+
+    public static Environment SetEnvironment()
+    {
+        var environment = new Environment(UpdVariables, ReadVariables);
+        return environment;
+    }
+
+    
+}
---
+++ PhysioADAPT/Assets/Environments/Environments/Demo/SphereBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Environments;
+
+public class SphereBehaviour : MonoBehaviour
+{
+    private static float SphereHeight = 0.5f;
+    private static bool CanLevitate;
+    
+    /// <summary>
+    /// Set variables as classes
+    /// </summary>
+    private void Start()
+    {
+        var height = new RangeVar("UB_SphereHeight", "float", SphereHeight.ToString(".00"), "0.5", "2.5");
+        Demo.SetUpdatableVariable(height);
+        var levitate = new BooleanVar("UB_CanLevitate", CanLevitate.ToString());
+        Demo.SetUpdatableVariable(levitate);
+    }
+
+    private void Update()
+    {
+        transform.position = CanLevitate ? new Vector3(transform.position.x, SphereHeight, transform.position.z) : new Vector3(transform.position.x, 0.5f, transform.position.z);
+    }
+    
+    /// <summary>
+    /// Call this function whenever the value of a variable changes
+    /// </summary>
+    /// <param name="var"></param>
+    public static void UpdateVariable(UpdatableVar var)
+    {
+        if (var.Name.Contains("SphereHeight"))
+            SphereHeight = float.Parse(var.Value);
+        else if (var.Name.Contains("CanLevitate"))
+            CanLevitate = bool.Parse(var.Value);
+    }
+}
