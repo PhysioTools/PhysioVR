8ce7cd21f1705b883867eefeb0bac8fbc40b3e3b
Teresa Paulino <teresampaulino@hotmail.com>
2016-06-12 17:12:34 +0100

    PhysioAdapter.cs and Sensor.cs created

---
+++ PhysioADAPT/Assets/Adapter/PhysioAdapter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Adapter
+{
+    public class PhysioAdapter : MonoBehaviour
+    {
+        public static List<Sensor> Sensors;
+        public static bool SensorAdded;
+
+        public static void SetSensor(string name, string type, string value)
+        {
+            if (Sensors.Count > 0)
+            {
+                for (var i = 0; i < Sensors.Count; i++)
+                {
+                    if (Sensors[i].Name == name)
+                    {
+                        Sensors[i].Value = value;
+                        SensorAdded = true;
+                    }
+                }
+                if (!SensorAdded)
+                {
+                    var newSensor = new Sensor(name, type, value);
+                    Sensors.Add(newSensor);
+                }
+            }
+            else
+            {
+                var newSensor = new Sensor(name, type, value);
+                Sensors.Add(newSensor);
+            }
+        }
+
+        public void ProcessData(string environment)
+        {
+            
+        }
+    }
+}
---
+++ PhysioADAPT/Assets/Adapter/Sensor.cs
@@ -0,0 +1,16 @@
+﻿namespace Assets.Adapter
+{
+    public class Sensor
+    {
+        public string Name;
+        public string Type;
+        public string Value;
+
+        public Sensor(string name, string type, string value)
+        {
+            Name = name;
+            Type = type;
+            Value = value;
+        }
+    }
+}
