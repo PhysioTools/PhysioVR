183a7db2e81fe4b1f858b3dfc1f4835357d6f1f0
Teresa Paulino <teresampaulino@hotmail.com>
2016-07-10 10:25:04 +0100

    Creation of the DataManager script for later implementation

---
+++ PhysioWOz/Assets/Manager/DataManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.Manager
+{
+    public class DataManager : MonoBehaviour {
+
+        public static void ParseData(string message)
+        {
+        
+        }
+    }
+}
